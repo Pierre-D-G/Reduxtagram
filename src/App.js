74c4dfee943700382c3df4a6ca0feaa878531d06
Pierre-D-G <gamil.pierre@gmail.com>
2017-06-27 12:11:18 -0400

    App Scaffold

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to Reduxtagram</p>
+      </div>
+    );
+  }
+}
+
+export default App;
