c6d08b0f48303223c99f851063afcf8ac41d7e13
Pierre-D-G <gamil.pierre@gmail.com>
2017-06-29 11:10:33 -0400

    Setup browser history

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
+
+import { Provider } from 'react-redux';
+
+import store, { history } from './store/store';
+
 import PhotoGrid from './components/PhotoGrid';
 import SinglePhoto from './components/SinglePhoto';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <h1>
-            <Link to='/'>
-              Reduxtagram
-            </Link>
-          </h1>
-          <Route exact path='/' component={PhotoGrid} />
-          <Route path='/view/:postId' component={SinglePhoto} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <div>
+              <h1>
+                <Link to='/'>
+                  Reduxtagram
+              </Link>
+              </h1>
+              <Route exact path='/' component={PhotoGrid} />
+              <Route path='/view/:postId' component={SinglePhoto} />
+            </div>
+          </Switch>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, compose, applyMiddleware } from 'redux';
-import { createHistory } from 'history/createHashHistory';
-import { routerMiddleware } from 'react-router-redux';
+import createBrowserHistory  from 'history/createBrowserHistory';
+import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 
 import posts from '../data/posts';
 import comments from '../data/comments';
@@ -14,9 +14,13 @@ const defaultState = {
     comments
 };
 
-const history = createHistory();
+const browserHistory = createBrowserHistory();
+const historyRouterMiddleware = routerMiddleware(browserHistory)
+
 const store = createStore(rootReducer, defaultState, compose(
-    applyMiddleware(routerMiddleware(history))
+    applyMiddleware(historyRouterMiddleware)
 ));
+
+export const history = syncHistoryWithStore(browserHistory, store)
 
 export default store;
