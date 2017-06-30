4d5b8949b093a32a5f7598622a6b5c290cfbc426
Pierre-D-G <gamil.pierre@gmail.com>
2017-06-30 17:51:57 -0400

    Created Comments componenent

---
+++ src/components/Comments.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+    render() {
+        return (
+            <div className="comment">
+                this is a comment
+            </div>
+        );
+    }
+}
+
+export default Comments;
--- src/components/SinglePhoto.js
+++ src/components/SinglePhoto.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import Photo from './Photo';
+import Comments from './Comments';
 
 class SinglePhoto extends Component {
   
   render() {
     const i = this.props.posts.findIndex((post) => post.code === this.props.match.params.postId);
-    console.log(i);
     const post = this.props.posts[i];
-    console.log(post);
     return (
       <div>
         <Home />
         <div className="single-photo">
           <Photo i={i} post={post} {...this.props}/>
+          <Comments />
         </div>
       </div>
     );
